5d581cd7ee90d962fd02dd926c4b8a6e11ba74b5
DESKTOP-BJ16ITI\hakon <hakon.elvik@gmail.com>
2025-01-24 15:25:50 +0100

    no message

--- The-money-clicker-game/script.js
+++ The-money-clicker-game/script.js
@@ -1,16 +1,41 @@
 let score=1;
-let bob =1
+let bob =1;
+let cost =10;
+let autoclick=1;
+let cost_auto=10;
+if (Event.key=="space")
+    scoreFun()
 function scoreFun(){
     score=score+bob
-    document.getElementById("score").innerHTML=score
-}
-function boben(){
-    if (score>10)
-        bob=bob+1
-        score=score-10
-        document.getElementById("score").innerHTML=score
-    if(score<10)
-        alert ("du har ikke nok money")
-        
     
 }
+function auto (){
+    score+=autoclick
+}
+function autoClick(){
+    if (score>(cost_auto-1)){
+        autoclick+=cost_auto/10
+        score-=cost_auto
+        cost_auto*=2
+    }
+    
+}
+function boben(){
+    if(score<cost){ 
+        alert ("du har ikke nok money")
+    }
+    if (score>(cost-1)){
+        bob=bob+cost/10
+        score=score-cost
+        cost*=2
+        
+    }
+    
+}
+function gameLoop(){
+    document.getElementById("cost").innerHTML="cost for 2x click power: "+cost
+    document.getElementById("score").innerHTML="money: "+score
+    document.getElementById("cost_auto").innerHTML="cost for 2x auto click : "+cost_auto
+}
+setInterval(gameLoop,100)
+setInterval(auto,1000)
