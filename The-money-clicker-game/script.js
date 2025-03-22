d99a5c51fd7fea52dfc5c67c9a823dba6f1e9f86
DESKTOP-BJ16ITI\hakon <hakon.elvik@gmail.com>
2025-03-22 13:48:31 +0100

    lagt til bit coin

--- The-money-clicker-game/script.js
+++ The-money-clicker-game/script.js
@@ -3,10 +3,20 @@ let bob =1;
 let cost =10;
 let autoclick=1;
 let cost_auto=10;
+let bitcoin=10;
+let bit_amout=10;
 if (Event.key=="space")
     scoreFun()
 function scoreFun(){
     score=score+bob
+    
+}
+function bitCoin(){
+    
+    bitcoin=bitcoin+Math.floor(Math.random()*21);
+    
+    bitcoin=bitcoin-8
+
     
 }
 function auto (){
@@ -18,8 +28,23 @@ function autoClick(){
         score-=cost_auto
         cost_auto*=2
     }
+}
+
+function buyBit(){
+    if (score>bitcoin){
+        score=score-bitcoin
+        bit_amout=bit_amout+1
+    }
     
 }
+function sellBit(){
+    if (bit_amout>0){
+        score=score+bitcoin
+        bit_amout=bit_amout-1
+    }
+}
+
+
 function boben(){
     if (score>(cost-1)){
         bob=bob+cost/10
@@ -33,6 +58,9 @@ function gameLoop(){
     document.getElementById("cost").innerHTML="cost for 2x click power: "+cost
     document.getElementById("score").innerHTML="money: "+score
     document.getElementById("cost_auto").innerHTML="cost for 2x auto click : "+cost_auto
+    document.getElementById("bit").innerHTML="bitcoin val : "+bitcoin
+    document.getElementById("bit_a").innerHTML="bitcoin amount : "+bit_amout
 }
 setInterval(gameLoop,100)
 setInterval(auto,1000)
+setInterval(bitCoin,1000)
